1f49e560a9d94945ad3c071bdabf1123c3566c37
EloyNavarro <eloynavarropoyato@gmail.com>
2025-05-23 14:01:47 +0200

    Delete Practica final java entregada - copia directory

--- Practica final java entregada - copia/java/menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const botonesAgregar = document.querySelectorAll(".agregar");
-
-    botonesAgregar.forEach(boton => {
-        boton.addEventListener("click", function() {
-            alert("Platillo agregado al pedido 🛒");
-        });
-    });
-});
